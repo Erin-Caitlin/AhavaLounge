f36f809be840da73b743b574731c79f5351c960b
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-08-28 16:01:11 +0200

    Footer

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,37 +1,16 @@
 <template>
     <nav class="navbar navbar-expand-lg sticky-top">
       <div class="container-fluid d-flex flex-column align-items-center">
-        <div class="navbar-title">
-          <h1>Ahava Lounge</h1>
-        </div>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+        <div class="navbar-left"><router-link class="navbar-brand" to="/" id="title"><i class="bi bi-person-fill"></i></router-link></div>
+        <div class="navbar-title"><h1>Ahava Lounge</h1></div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
           <ul class="navbar-nav mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/about">About</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/movies">Movie Deals</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/booking">Book Now</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/contact">Contact</router-link>
-            </li>
+            <li class="nav-item"><router-link class="nav-link" to="/">Home</router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/about">About</router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/movies">Movie Deals</router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/booking">Book Now</router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/contact">Contact</router-link></li>
           </ul>
         </div>
       </div>
@@ -46,22 +25,27 @@
   
   <style scoped>
   .navbar {
-    background-color: #f8f9fa; 
+    background-color: #f8f9fa;
+    padding-top: 10px;
   }
   
- 
+
+  .navbar-left {
+    position: absolute;
+    left: 20px;
+    top: 15px;
+  }
+  
   .navbar-title {
-    margin-bottom: 10px; 
+    margin-bottom: 10px;
   }
   
   .navbar-title h1 {
-    font-size: 24px; 
-    color: #343a40; 
+    font-size: 24px;
+    color: #343a40;
     text-align: center;
-    margin: 0;       
+    margin: 0
   }
-  
-  
   .navbar-nav {
     justify-content: center;
   }
@@ -79,10 +63,9 @@
     background-color: #e2e6ea;
   }
   
-  /* Mobile adjustments */
   @media (max-width: 991.98px) {
     .navbar-title {
-      margin-bottom: 15px; 
+      margin-bottom: 15px;
     }
     
     .navbar-toggler {
@@ -90,23 +73,23 @@
     }
     
     .navbar-collapse {
-      position: fixed;        
-      top: 0;                 
-      left: -250px;          
-      width: 250px;          
-      height: 100%;        
-      background-color: #343a40; 
-      transition: left 0.3s ease; 
+      position: fixed;
+      top: 0;
+      left: -250px;
+      width: 250px;
+      height: 100%;
+      background-color: #343a40;
+      transition: left 0.3s ease;
       z-index: 1050;
     }
   
     .navbar-collapse.show {
-      left: 0; 
+      left: 0;
     }
   
     .nav-link {
-      color: #fff; 
-      padding: 15px 20px; 
+      color: #fff;
+      padding: 15px 20px;
     }
   }
   </style>
