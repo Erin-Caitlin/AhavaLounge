8481c446b51cee72a3ca8fc60fa424eb27e1882f
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-08-29 09:44:33 +0200

    Styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <video autoplay muted loop class="background-video">
-      <source src="/videos/background.mp4" type="video/mp4">
+      <source src="https://erin-caitlin.github.io/AhavaImages/videos/backgroundvideo.mp4" type="video/mp4">
       Your browser does not support the video tag.
     </video>
     <div class="content">
