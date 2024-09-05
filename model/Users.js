53f50a4e02a29cd7f87b059a86bcd792de28b928
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-09-05 09:37:14 +0200

    err

--- model/Users.js
+++ model/Users.js
@@ -10,7 +10,7 @@ class Users {
             FROM Users;
             `
             db.query(strQry, (err, results) => {
-                if(err) throw new Error('Unable to fetch user')
+                if(err) throw new Error(err)
                     res.json({
                 status: res.statusCode,
                         results    
